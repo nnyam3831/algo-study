33c3833a71a72ac620318b37740f1fbbb00dd3fe
seongwon <nnyam3831@naver.com>
2021-01-22 14:15:18 +0900

    leetcode LCA

---
+++ leetcode/236/solve.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(
+        self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
+    ) -> "TreeNode":
+        if root in (None, p, q):
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = slef.lowestCommonAncestor(root.right, p, q)
+        return root if right and left else right or left
